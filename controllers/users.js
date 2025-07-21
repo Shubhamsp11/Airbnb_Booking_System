83fe0ca8693a993fe4d9a39047c6dab59754f05c
shubhamsp1109@gmail.com <shubhamsp1109@gmail.com>
2025-07-21 23:06:22 +0530

    update

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup=async(req,res)=>{
     if(err){
         return next(err);
     }
-    req.flash("success","welcome to wanderlust!");
+    req.flash("success","welcome!");
    res.redirect("/listings");
    })
    
