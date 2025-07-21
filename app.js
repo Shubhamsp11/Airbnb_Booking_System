6213f79e5381f04ce3081bcd95449352501bc68e
shubhamsp1109@gmail.com <shubhamsp1109@gmail.com>
2025-07-21 23:19:21 +0530

    redirected

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ async function main(){
     await mongoose.connect(dbUrl);
 }
 
-app.set("view engine ","ejs");
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
@@ -127,7 +127,7 @@ app.use("/",userRouter);
 
 
 app.get("/", (req, res) => {
-    res.send("/listings"); 
+    res.redirect("/listings"); 
 });
 
 
